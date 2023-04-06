d62e37e62686e0ee0d61f3c3eed53b5a574dd3c0
victory1743123862 <117185160+victory1743123862@users.noreply.github.com>
2023-04-06 19:28:07 +0800

    判断语句使用

---
+++ test1.py
@@ -0,0 +1,11 @@
+# -*- code =utf-8 -*-
+# @Time :2023/4/616:56
+# @Auther :世界上最帅的男人
+# @File :test1.py
+# @Software :PyCharm
+password = input("请输入")
+trueword = "123456"
+if trueword == password:
+    print("登录成功")
+elif trueword != password:
+    print("输入错误")
