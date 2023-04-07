ad3c8a30440f3fb18c656d371aadc7b0f0398548
victory1743123862 <117185160+victory1743123862@users.noreply.github.com>
2023-04-07 20:53:19 +0800

    found 循环

---
+++ test3.py
@@ -0,0 +1,21 @@
+# -*- code =utf-8 -*-
+# @Time :2023/4/719:45
+# @Auther :世界上最帅的男人
+# @File :test3.py
+# @Software :PyCharm
+
+for i in range(5):
+    print(i)
+
+a = ['aa', 'bb', 'cc', 'dd']
+for i in range(len(a)):
+    print(i, a[i])
+
+name = 'zhangye'
+for i in name:
+    print(i, end='\t')
+
+
+
+
+
