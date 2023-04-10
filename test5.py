7218abcb57ccc9e99a9b3992da81bb5e7ad40a46
victory1743123862 <117185160+victory1743123862@users.noreply.github.com>
2023-04-10 19:04:16 +0800

    found 字符串

---
+++ test5.py
@@ -0,0 +1,21 @@
+# -*- code =utf-8 -*-
+# @Time :2023/4/1017:14
+# @Auther :世界上最帅的男人
+# @File :test5.py
+# @Software :PyCharm
+
+
+# 字符串
+a = 'dvsfdv'
+b = "这是一个句子"
+c = """
+这是一个段落
+可以写一段话
+"""
+print(a)
+print(b)
+print(c)
+
+str1 = "zhangye"
+print(str1[3:5] )
+print(str1[2:5:2])
